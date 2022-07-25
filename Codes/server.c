7b7f0cc9606d8bf465ad9e53e7a0da488c5b50f8
Kelvin Araújo Ferreira <dilli.kelvin@gmail.com>
2022-07-25 15:29:23 -0400

    Update server.c

--- Codes/server.c
+++ Codes/server.c
@@ -4,11 +4,8 @@
 
 #define WIN32_LEAN_AND_MEAN
 
-#include <winsock2.h>
 #include <Ws2tcpip.h>
-#include <stdio.h>
 
-// Link with ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
 
 // ------------------------------
